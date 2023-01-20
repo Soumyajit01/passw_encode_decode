893f694952f36d3969d65ee17949e59d0deef544
Soumyajit01 <92006607+Soumyajit01@users.noreply.github.com>
2023-01-21 04:12:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import random
 import string
 letters = string.ascii_lowercase
+characters = ['!','@','#','$','%','^','&','*','_','.']
+random.shuffle(characters)
+def encode(passw):
+    passw=list(passw[::-1]) # reversing the sequence
+    start_chars=''.join(random.choices(letters,k=3))
+    end_chars=''.join(random.choices(letters,k=3))
+    hashed_list = [x for pair in zip(passw, characters) for x in pair]
+    mid_chars=''.join(hashed_list)
+    return start_chars+mid_chars+end_chars
 
-def encode(text):
-    if len(text)>=3:
-        start_random = random.choices(letters,k=3)
-        end_random = random.choices(letters,k=3)
-        text=text+text[0]
-        text=text.replace(text[0],'',1)
-        text=(''.join(start_random))+text+(''.join(end_random))
-    elif len(text)<3:
-        text=text[::-1]
-    return text
+    
 def decode(text):
-    if len(text)<3:
-        text=text[::-1]
-    else:
-        text=text[3:-3]
-        text=(text[len(text)-1]+text)[:-1]
-    return text
+    pass
